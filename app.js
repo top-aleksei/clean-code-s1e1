40c1135b79560752801f21c94a253c3c73079a63
Aleksei <toporov.aleksei@gmail.com>
2022-10-28 23:26:21 +0300

    refactor: add alternative content for media (p 1.2) according to html-and-css-extended.md

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.classList.add("button", "delete");
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="delete__logo"
+    deleteButtonImg.alt='Delete'
     deleteButton.appendChild(deleteButtonImg);
 
 
